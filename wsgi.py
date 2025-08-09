53613aaa0db8310d2c44e6c2f03e24bfbfef5d6c
anon94835-creator <anon94835@gmail.com>
2025-08-09 16:42:56 +0800

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -2,5 +2,5 @@ import os
 from app import app  # Make sure 'app' is imported from your Flask application file
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))  # Default to 5000 if PORT not set
+    port = int(os.environ.get("PORT", 4000))  # Default to 5000 if PORT not set
     app.run(host="0.0.0.0", port=port)  # Fixed parameter names (host/port)
